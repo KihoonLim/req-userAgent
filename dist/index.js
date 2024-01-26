ed340c2d1bcdc412affb99eb3d0b033738566309
ricky <khim89@naver.com>
2024-01-26 10:46:35 +0900

    versionup

--- dist/index.js
+++ dist/index.js
@@ -1,162 +1,168 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetClientInfo = void 0;
+exports.parseUserAgent = exports.GetClientInfo = void 0;
 const GetClientInfo = (req) => {
     try {
-        const unknown = '-';
-        //browser
-        const nVer = req.appVersion;
-        const nAgt = req.headers['user-agent'] || '';
-        let browser = 'UNDEFINED';
-        let version = '';
-        let nameOffset, verOffset, ix;
-        // Opera
-        if ((verOffset = nAgt.indexOf('Opera')) != -1) {
-            browser = 'Opera';
-            version = nAgt.substring(verOffset + 6);
-            if ((verOffset = nAgt.indexOf('Version')) != -1) {
-                version = nAgt.substring(verOffset + 8);
-            }
-        }
-        // MSIE
-        else if ((verOffset = nAgt.indexOf('MSIE')) != -1) {
-            browser = 'Microsoft Internet Explorer';
-            version = nAgt.substring(verOffset + 5);
-        }
-        // Whale
-        else if ((verOffset = nAgt.indexOf('Whale')) != -1) {
-            browser = 'Whale';
-            version = nAgt.substring(verOffset + 6);
-        }
-        // Edge
-        else if ((verOffset = nAgt.indexOf('Edg')) != -1) {
-            browser = 'Microsoft Edge';
-            version = nAgt.substring(verOffset + 4);
-        }
-        // SamsungBrowser
-        else if ((verOffset = nAgt.indexOf('SamsungBrowser')) != -1) {
-            browser = 'SamsungBrowser';
-            version = nAgt.substring(verOffset + 15);
-        }
-        // NAVER
-        else if ((verOffset = nAgt.indexOf('NAVER')) != -1) {
-            browser = 'NAVER';
-            version = nAgt.substring(verOffset + 26);
-        }
-        // KAKAOTALK
-        else if ((verOffset = nAgt.indexOf('KAKAOTALK')) != -1) {
-            browser = 'KAKAOTALK';
-            version = nAgt.substring(verOffset + 10);
-        }
-        // Chrome
-        else if ((verOffset = nAgt.indexOf('Chrome')) != -1) {
-            browser = 'Chrome';
-            version = nAgt.substring(verOffset + 7);
-        }
-        // Safari
-        else if ((verOffset = nAgt.indexOf('Safari')) != -1) {
-            browser = 'Safari';
-            version = nAgt.substring(verOffset + 7);
-            if ((verOffset = nAgt.indexOf('Version')) != -1) {
-                version = nAgt.substring(verOffset + 8);
-            }
-        }
-        // Firefox
-        else if ((verOffset = nAgt.indexOf('Firefox')) != -1) {
-            browser = 'Firefox';
-            version = nAgt.substring(verOffset + 8);
-        }
-        // MSIE 11+
-        else if (nAgt.indexOf('Trident/') != -1) {
-            browser = 'Microsoft Internet Explorer';
-            version = nAgt.substring(nAgt.indexOf('rv:') + 3);
-        }
-        // Other browsers
-        else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
-            (verOffset = nAgt.lastIndexOf('/'))) {
-            browser = nAgt.substring(nameOffset, verOffset);
-            version = nAgt.substring(verOffset + 1);
-        }
-        // trim the version string
-        if ((ix = version.indexOf(';')) != -1) {
-            version = version.substring(0, ix);
-        }
-        if ((ix = version.indexOf(' ')) != -1) {
-            version = version.substring(0, ix);
-        }
-        if ((ix = version.indexOf(')')) != -1) {
-            version = version.substring(0, ix);
-        }
-        // system
-        let os = unknown;
-        const clientStrings = [
-            { s: 'Windows 10', r: /(Windows 10.0|Windows NT 10.0)/ },
-            { s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/ },
-            { s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/ },
-            { s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/ },
-            { s: 'Windows Vista', r: /Windows NT 6.0/ },
-            { s: 'Windows Server 2003', r: /Windows NT 5.2/ },
-            { s: 'Windows XP', r: /(Windows NT 5.1|Windows XP)/ },
-            { s: 'Windows 2000', r: /(Windows NT 5.0|Windows 2000)/ },
-            { s: 'Windows ME', r: /(Win 9x 4.90|Windows ME)/ },
-            { s: 'Windows 98', r: /(Windows 98|Win98)/ },
-            { s: 'Windows 95', r: /(Windows 95|Win95|Windows_95)/ },
-            { s: 'Windows NT 4.0', r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/ },
-            { s: 'Windows CE', r: /Windows CE/ },
-            { s: 'Windows 3.11', r: /Win16/ },
-            { s: 'Android', r: /Android/ },
-            { s: 'Open BSD', r: /OpenBSD/ },
-            { s: 'Sun OS', r: /SunOS/ },
-            { s: 'Linux', r: /(Linux|X11)/ },
-            { s: 'iOS', r: /(iPhone|iPad|iPod)/ },
-            { s: 'Mac OS X', r: /Mac OS X/ },
-            { s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ },
-            { s: 'QNX', r: /QNX/ },
-            { s: 'UNIX', r: /UNIX/ },
-            { s: 'BeOS', r: /BeOS/ },
-            { s: 'OS/2', r: /OS\/2/ },
-            {
-                s: 'Search Bot',
-                r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/,
-            },
-        ];
-        for (const id in clientStrings) {
-            const cs = clientStrings[id];
-            if (cs.r.test(nAgt)) {
-                os = cs.s;
-                break;
-            }
-        }
-        let osVersion = unknown;
-        if (/Windows/.test(os)) {
-            osVersion = /Windows (.*)/.exec(os)[1];
-            os = 'Windows';
-        }
-        switch (os) {
-            case 'Mac OS X':
-                osVersion = /Mac OS X (\d+[\.\_\d]+)/.exec(nAgt)[1];
-                break;
-            case 'Android':
-                osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
-                break;
-            case 'iOS':
-                osVersion = /OS ([\.\_\d]+)/.exec(nAgt)[1];
-                break;
-        }
+        const nAgt = req.headers["user-agent"] || "";
+        const { os, osVersion, browser, browserVersion } = (0, exports.parseUserAgent)(nAgt);
         const detectInfo = {
-            ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-            browser: browser,
-            browserVersion: version,
+            ip: req.headers["x-forwarded-for"] || req.connection.remoteAddress,
+            browser,
+            browserVersion,
             os: os,
-            osVersion: osVersion,
+            osVersion,
         };
         return detectInfo;
     }
     catch (error) {
         return {
-            error
+            error,
         };
     }
 };
 exports.GetClientInfo = GetClientInfo;
+const parseUserAgent = (userAgent) => {
+    const unknown = "-";
+    const _userAgent = userAgent || "";
+    let browser = "UNDEFINED";
+    let browserVersion = "";
+    let nameOffset, verOffset;
+    // Opera
+    if ((verOffset = _userAgent.indexOf("Opera")) != -1) {
+        browser = "Opera";
+        browserVersion = _userAgent.substring(verOffset + 6);
+        if ((verOffset = _userAgent.indexOf("Version")) != -1) {
+            browserVersion = _userAgent.substring(verOffset + 8);
+        }
+    }
+    // MSIE
+    else if ((verOffset = _userAgent.indexOf("MSIE")) != -1) {
+        browser = "Microsoft Internet Explorer";
+        browserVersion = _userAgent.substring(verOffset + 5);
+    }
+    // Whale
+    else if ((verOffset = _userAgent.indexOf("Whale")) != -1) {
+        browser = "Whale";
+        browserVersion = _userAgent.substring(verOffset + 6);
+    }
+    // Edge
+    else if ((verOffset = _userAgent.indexOf("Edg")) != -1) {
+        browser = "Microsoft Edge";
+        browserVersion = _userAgent.substring(verOffset + 4);
+    }
+    // SamsungBrowser
+    else if ((verOffset = _userAgent.indexOf("SamsungBrowser")) != -1) {
+        browser = "SamsungBrowser";
+        browserVersion = _userAgent.substring(verOffset + 15);
+    }
+    // NAVER
+    else if ((verOffset = _userAgent.indexOf("NAVER")) != -1) {
+        browser = "NAVER";
+        browserVersion = _userAgent.substring(verOffset + 26);
+    }
+    // KAKAOTALK
+    else if ((verOffset = _userAgent.indexOf("KAKAOTALK")) != -1) {
+        browser = "KAKAOTALK";
+        browserVersion = _userAgent.substring(verOffset + 10);
+    }
+    // Chrome
+    else if ((verOffset = _userAgent.indexOf("Chrome")) != -1) {
+        browser = "Chrome";
+        browserVersion = _userAgent.substring(verOffset + 7);
+    }
+    // Safari
+    else if ((verOffset = _userAgent.indexOf("Safari")) != -1) {
+        browser = "Safari";
+        browserVersion = _userAgent.substring(verOffset + 7);
+        if ((verOffset = _userAgent.indexOf("Version")) != -1) {
+            browserVersion = _userAgent.substring(verOffset + 8);
+        }
+    }
+    // Firefox
+    else if ((verOffset = _userAgent.indexOf("Firefox")) != -1) {
+        browser = "Firefox";
+        browserVersion = _userAgent.substring(verOffset + 8);
+    }
+    // MSIE 11+
+    else if (_userAgent.indexOf("Trident/") != -1) {
+        browser = "Microsoft Internet Explorer";
+        browserVersion = _userAgent.substring(_userAgent.indexOf("rv:") + 3);
+    }
+    // Other browsers
+    else if ((nameOffset = _userAgent.lastIndexOf(" ") + 1) <
+        (verOffset = _userAgent.lastIndexOf("/"))) {
+        browser = _userAgent.substring(nameOffset, verOffset);
+        browserVersion = _userAgent.substring(verOffset + 1);
+    }
+    browserVersion = browserVersion.replace(/(;|\s|\)).*/, "");
+    const clientOsMatcher = [
+        { os: "Windows", r: /(Windows 10.0|Windows NT 10.0)/, v: "10" },
+        { os: "Windows", r: /(Windows 8.1|Windows NT 6.3)/, v: "8.1" },
+        { os: "Windows", r: /(Windows 8|Windows NT 6.2)/, v: "8" },
+        { os: "Windows", r: /(Windows 7|Windows NT 6.1)/, v: "7" },
+        { os: "Windows", r: /Windows NT 6.0/, v: "Vista" },
+        { os: "Windows", r: /Windows NT 5.2/, v: "Server 2003" },
+        { os: "Windows", r: /(Windows NT 5.1|Windows XP)/, v: "XP" },
+        { os: "Windows", r: /(Windows NT 5.0|Windows 2000)/, v: "2000" },
+        { os: "Windows", r: /(Win 9x 4.90|Windows ME)/, v: "ME" },
+        { os: "Windows", r: /(Windows 98|Win98)/, v: "98" },
+        { os: "Windows", r: /(Windows 95|Win95|Windows_95)/, v: "95" },
+        {
+            os: "Windows",
+            r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/,
+            v: "NT 4.0",
+        },
+        { os: "Windows", r: /Windows CE/, v: "CE" },
+        { os: "Windows", r: /Win16/, v: "3.11" },
+        {
+            os: "Android",
+            r: /Android/,
+            getV: () => /Android ([\.\_\d]+)/.exec(_userAgent)[1],
+        },
+        { os: "Open BSD", r: /OpenBSD/ },
+        { os: "Sun OS", r: /SunOS/ },
+        { os: "Linux", r: /(Linux|X11)/ },
+        {
+            os: "iOS",
+            r: /(iPhone|iPad|iPod)/,
+            getV: () => /OS ([\.\_\d]+)/.exec(_userAgent)[1],
+        },
+        {
+            os: "Mac OS X",
+            r: /Mac OS X/,
+            getV: () => /Mac OS X (\d+[\.\_\d]+)/.exec(_userAgent)[1],
+        },
+        { os: "Mac OS", r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ },
+        { os: "QNX", r: /QNX/ },
+        { os: "UNIX", r: /UNIX/ },
+        { os: "BeOS", r: /BeOS/ },
+        { os: "OS/2", r: /OS\/2/ },
+        {
+            os: "Search Bot",
+            r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/,
+        },
+    ];
+    const matched = clientOsMatcher.find((it) => it.r.test(_userAgent));
+    const os = matched ? matched.os : unknown;
+    let osVersion;
+    switch (os) {
+        case "Mac OS X":
+        case "Android":
+        case "iOS":
+            osVersion = matched.getV();
+            break;
+        case "Windows":
+            osVersion = matched.v;
+            break;
+        default:
+            osVersion = unknown;
+    }
+    return {
+        os,
+        osVersion,
+        browser,
+        browserVersion,
+    };
+};
+exports.parseUserAgent = parseUserAgent;
 //# sourceMappingURL=index.js.map
